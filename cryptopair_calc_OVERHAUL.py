111c9ef5375dda28a3be7e9255eea817adbfd9b1
hmallen <allenhm@gmail.com>
2017-07-21 03:17:56 -0400

    Functional calculator after total overhaul.

--- cryptopair_calc_OVERHAUL.py
+++ cryptopair_calc_OVERHAUL.py
@@ -9,7 +9,24 @@ import time
 public_client = gdax.PublicClient()
 
 
-def display_current():
+def user_menu(command):
+    if command == 0:
+        print('Stuff')
+    elif command == 1:
+        print('Things')
+    elif command == 2:
+        print('Junk')
+    else:
+        print('Error!')
+
+
+def get_prices():
+    global btcusd_bid, btcusd_ask
+    global ltcusd_bid, ltcusd_ask
+    global ltcbtc_bid, ltcbtc_ask
+    global ethusd_bid, ethusd_ask
+    global ethbtc_bid, ethusd_ask
+
     # BTCUSD
     btcusd_bidask = public_client.get_product_order_book('BTC-USD', level=1)
 
@@ -40,12 +57,19 @@ def display_current():
     ethbtc_bid = ethbtc_bidask['bids'][0][0]
     ethbtc_ask = ethbtc_bidask['asks'][0][0]
 
-    # CALCULATE ARBITRAGE OPPORTUNITIES
-    ltcusd_viabtc = float(ltcbtc_ask) * float(btcusd_ask)
-    ltc_diff = float(ltcusd_viabtc) - float(ltcusd_ask)
 
-    ethusd_viabtc = float(ethbtc_ask) * float(btcusd_ask)
-    eth_diff = float(ethusd_viabtc) - float(ethusd_ask)
+def display_current():
+    global btcusd_bid, btcusd_ask
+    global ltcusd_bid, ltcusd_ask
+    global ltcbtc_bid, ltcbtc_ask
+    global ethusd_bid, ethusd_ask
+    global ethbtc_bid, ethusd_ask
+
+    ltcusd_viabtc = ltcbtc_ask * btcusd_ask
+    ltc_diff = ltcusd_viabtc - ltcusd_ask
+
+    ethusd_viabtc = ethbtc_ask * btcusd_ask
+    eth_diff = ethusd_viabtc - ethusd_ask
 
     print()
     print('LTCUSD:  ' + str(ltcusd_ask))
@@ -55,19 +79,15 @@ def display_current():
     print('ETCUSD:  ' + str(ethusd_ask))
     print('E-B-USD: ' + str(ethusd_viabtc))
     print('ETH ARB: ' + str(eth_diff))
-    print()
     print('--------------------------------')
 
 
 try:
     while True:
+        get_prices()
         display_current()
         time.sleep(5)
-
-except KeyboardInterrupt:
-    print('Exiting program.')
-    sys.exit()
         
 except:
-    print('Exiting program with exception.')
+    print('Exiting program.')
     sys.exit()
